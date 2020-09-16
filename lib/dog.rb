94c63a29e69a6c38b4c9b4f9c9d2a4aa42596415
Valeria  Copleman <valeriacopleman@gmail.com>
2020-09-16 16:38:29 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,13 @@
 class Dog 
   
-  def give_name(name)
-     = Dog.new 
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
+  end
+ 
+  def name
+    @this_dogs_name
+  end
+end 
     
   def bark
     puts "woof!"
